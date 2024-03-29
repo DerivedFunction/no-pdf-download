9a33a484ac25fd40df1464c134a74590a29d1cf2
del226 <del226@lehigh.edu>
2024-03-29 10:13:13 -0400

    added formatting

--- app/src/background.js
+++ app/src/background.js
@@ -1,11 +1,10 @@
-'use strict';
+"use strict";
 
-let extraInfoSpec = [
-    'responseHeaders',
-    'blocking',
-];
-if (chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty('EXTRA_HEADERS')) {
-    extraInfoSpec.push('extraHeaders');
+let extraInfoSpec = ["responseHeaders", "blocking"];
+if (
+    chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty("EXTRA_HEADERS")
+) {
+    extraInfoSpec.push("extraHeaders");
 }
 
 chrome.webRequest.onHeadersReceived.addListener(
@@ -13,16 +12,13 @@ chrome.webRequest.onHeadersReceived.addListener(
         let newHeaders = handleHeaders(details.url, details.responseHeaders);
         if (newHeaders !== undefined) {
             return {
-                responseHeaders: newHeaders
+                responseHeaders: newHeaders,
             };
         }
     },
     {
-        types: [
-            'main_frame',
-            'sub_frame',
-        ],
-        urls: ['<all_urls>'],
+        types: ["main_frame", "sub_frame"],
+        urls: ["<all_urls>"],
     },
-    extraInfoSpec
+    extraInfoSpec,
 );
