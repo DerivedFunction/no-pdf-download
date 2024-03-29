fa29f8e462fa8cab1d68d63b530c354f290283c4
DerivedFunction <81781562+DerivedFunction@users.noreply.github.com>
2024-03-28 22:23:38 -0400

    Update background.js

--- app/src/background.js
+++ app/src/background.js
@@ -1,19 +1,17 @@
 'use strict';
 
-// For Chrome we have to use "extraHeaders" to get all headers
 let extraInfoSpec = [
     'responseHeaders',
     'blocking',
 ];
-if(chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty('EXTRA_HEADERS')) {
+if (chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty('EXTRA_HEADERS')) {
     extraInfoSpec.push('extraHeaders');
 }
 
-// Register receiver for reponse headers
 chrome.webRequest.onHeadersReceived.addListener(
     (details) => {
         let newHeaders = handleHeaders(details.url, details.responseHeaders);
-        if(newHeaders !== undefined) {
+        if (newHeaders !== undefined) {
             return {
                 responseHeaders: newHeaders
             };
